d9ba937a069e355191d2f41e5dfbfb70ba4a116c
Angel0v22w <velislavangelov19@gmail.com>
2023-12-29 14:23:19 +0200

    Database testing

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
 
 
 # Internationalization
